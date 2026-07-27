8530e3eb804edd51ee9ad74018f6cc73ffecde22
Perri Luo <perriluo05-lagom@users.noreply.github.com>
2026-07-27 19:25:53 +0800

    cleanup: remove temporary deployment scripts

--- scripts/deploy.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "d:\Trae CN\program\follow-builders"
-D:\Git\cmd\git.exe pull
-D:\Git\cmd\git.exe add -A
-D:\Git\cmd\git.exe commit -m "chore: update to Chinese-only, remove procedural summarization"
-D:\Git\cmd\git.exe push
-echo Deployment completed
-pause
